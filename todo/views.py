1ddefc68b442e025081d1cce272cfed43ec11cd3
Ozodbek <ahmadovozodbek80@gmail.com>
2025-02-04 22:49:52 +0500

    The project finished.

--- todo/views.py
+++ todo/views.py
@@ -1,17 +1,12 @@
 from django.urls import reverse_lazy
 from django.views import generic
-from .models import Task
 from .forms import TaskForm
-from django.db.models import Q
-from django.views.generic import ListView
 from .models import Task
-import logging
-
-
 import logging
 logger = logging.getLogger(__name__)
 
-class TaskListView(ListView):
+
+class TaskListView(generic.ListView):
     model = Task
     template_name = 'todo/list.html'
     context_object_name = 'tasks'
@@ -19,34 +14,28 @@ class TaskListView(ListView):
     def get_queryset(self):
         queryset = super().get_queryset()
 
-        # Search filter
         search = self.request.GET.get('search', '')
         if search:
             logger.debug(f"Search filter applied: {search}")
             queryset = queryset.filter(title__icontains=search)
-
-        # Priority filter
         priority = self.request.GET.get('priority')
+
         if priority:
             logger.debug(f"Priority filter applied: {priority}")
             queryset = queryset.filter(priority=priority)
 
-        # Status filter
         status = self.request.GET.get('status')
         if status:
             logger.debug(f"Status filter applied: {status}")
             queryset = queryset.filter(status__iexact=status)
 
-        # Order by filter
         order_by = self.request.GET.get('order_by')
         if order_by:
             logger.debug(f"Order by filter applied: {order_by}")
             if order_by in ['due_date', 'priority', 'status']:
                 queryset = queryset.order_by(order_by)
 
-        # Log the final queryset
         logger.debug(f"Final queryset: {queryset.query}")
-
         return queryset
 
 
